f5b0b96ae24b7502a129aa6a2b89dcd175e2eb87
jdeepankur <jdeepankur@outlook.com>
2026-03-20 16:34:39 +0000

    file upload

--- Encapsulation/src/main/java/Encapsulation.java
+++ Encapsulation/src/main/java/Encapsulation.java
@@ -1,3 +1,5 @@
+
+
 public class Encapsulation {
     public static void main(String[] args) {
         Person p = new Person();
@@ -9,4 +11,85 @@ class Person {
     private String name = "";
     private int age = 0;
     private String country = "";
+
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCountry() {
+        return this.country;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    } 
+}
+
+class BankAccount {
+    private long accountNumber;
+    private int balance;
+
+    public long getAccountNumber() {
+        return this.accountNumber;
+    }
+    public void setAccountNumber(long accountNumber) {
+        if (accountNumber > 1e7) {
+            this.accountNumber = accountNumber;
+        }
+        else {
+            throw new RuntimeException("Account Number is usually 8 digits.");
+        }
+    }
+
+    public float getBalance() {
+        return (float) this.balance / 100;
+    }
+    public void setBalance(float balance) {
+        if (balance >= 0) {
+            this.balance = (int) (balance * 100);
+        }
+        else {
+            throw new RuntimeException("This bank does not allow overdrafts!");
+        }
+    } 
+}
+
+public class Rectangle {
+    private int length = 0;
+    private int width = 0;
+
+    public int getLength() {
+        return this.length;
+    }
+    public void setLength(int length) {
+        if (length > 0) {
+            this.length = length;
+        }
+        else {
+            throw new RuntimeException("Length must be a positive integer.");
+        }
+    }
+
+    public int getLength() {
+        return this.length;
+    }
+    public void setLength(int length) {
+        if (length > 0) {
+            this.length = length;
+        }
+        else {
+            throw new RuntimeException("Length must be a positive integer.");
+        }
+    }
+
+    public getArea() {}
 }
--- Encapsulation/src/test/java/EncapsulationTest.java
+++ Encapsulation/src/test/java/EncapsulationTest.java
@@ -3,6 +3,8 @@ import org.junit.jupiter.api.Test;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class EncapsulationTest {
@@ -13,5 +15,200 @@ public class EncapsulationTest {
             assertTrue(Modifier.isPrivate(field.getModifiers()),
                     "Field '" + field.getName() + "' should be private");
         }
+    }
+
+    @Test
+    public void testNameGetterAndSetter() {
+        Person person = new Person();
+        String expectedName = "John Doe";
+        person.setName(expectedName);
+        assertEquals(expectedName, person.getName());
+    }
+
+    @Test
+    public void testAgeGetterAndSetter() {
+        Person person = new Person();
+        int expectedAge = 25;
+        person.setAge(expectedAge);
+        assertEquals(expectedAge, person.getAge());
+    }
+
+    @Test
+    public void testCountryGetterAndSetter() {
+        Person person = new Person();
+        String expectedCountry = "USA";
+        person.setCountry(expectedCountry);
+        assertEquals(expectedCountry, person.getCountry());
+    }
+
+    @Test
+    public void testAllBankAccountFieldsArePrivate() {
+        Field[] fields = BankAccount.class.getDeclaredFields();
+        for (Field field : fields) {
+            assertTrue(Modifier.isPrivate(field.getModifiers()),
+                    "Field '" + field.getName() + "' should be private");
+        }
+    }
+
+    @Test
+    public void testSetAccountNumberWithValidValue() {
+        BankAccount account = new BankAccount();
+        long validAccountNumber = 12345678L;
+        account.setAccountNumber(validAccountNumber);
+        assertEquals(validAccountNumber, account.getAccountNumber());
+    }
+
+    @Test
+    public void testSetAccountNumberWithLargeValidValue() {
+        BankAccount account = new BankAccount();
+        long largeAccountNumber = 99999999L;
+        account.setAccountNumber(largeAccountNumber);
+        assertEquals(largeAccountNumber, account.getAccountNumber());
+    }
+
+    @Test
+    public void testSetAccountNumberWithMinimumBoundaryValue() {
+        BankAccount account = new BankAccount();
+        long boundaryValue = (long)(1e7 + 1);
+        account.setAccountNumber(boundaryValue);
+        assertEquals(boundaryValue, account.getAccountNumber());
+    }
+
+    @Test
+    public void testSetAccountNumberWithInvalidValue() {
+        BankAccount account = new BankAccount();
+        long invalidAccountNumber = 1234567L;
+        assertThrows(RuntimeException.class, () -> {
+            account.setAccountNumber(invalidAccountNumber);
+        });
+    }
+
+    @Test
+    public void testSetAccountNumberWithZero() {
+        BankAccount account = new BankAccount();
+        assertThrows(RuntimeException.class, () -> {
+            account.setAccountNumber(0L);
+        });
+    }
+
+    @Test
+    public void testSetAccountNumberWithNegativeValue() {
+        BankAccount account = new BankAccount();
+        assertThrows(RuntimeException.class, () -> {
+            account.setAccountNumber(-12345678L);
+        });
+    }
+
+    @Test
+    public void testGetAccountNumber() {
+        BankAccount account = new BankAccount();
+        long expectedValue = 98765432L;
+        account.setAccountNumber(expectedValue);
+        assertEquals(expectedValue, account.getAccountNumber());
+    }
+
+    @Test
+    public void testSetBalanceWithValidValue() {
+        BankAccount account = new BankAccount();
+        float validBalance = 5000.50f;
+        account.setBalance(validBalance);
+        assertEquals(validBalance, account.getBalance());
+    }
+
+    @Test
+    public void testSetBalanceWithZero() {
+        BankAccount account = new BankAccount();
+        account.setBalance(0.00f);
+        assertEquals(0.00f, account.getBalance());
+    }
+
+    @Test
+    public void testSetBalanceWithLargeValue() {
+        BankAccount account = new BankAccount();
+        float largeBalance = 99999.99f;
+        account.setBalance(largeBalance);
+        assertEquals(largeBalance, account.getBalance());
+    }
+
+    @Test
+    public void testSetBalanceWithCents() {
+        BankAccount account = new BankAccount();
+        float balanceWithCents = 100.50f;
+        account.setBalance(balanceWithCents);
+        assertEquals(balanceWithCents, account.getBalance());
+    }
+
+    @Test
+    public void testSetBalanceWithSingleCent() {
+        BankAccount account = new BankAccount();
+        float singleCent = 0.01f;
+        account.setBalance(singleCent);
+        assertEquals(singleCent, account.getBalance());
+    }
+
+    @Test
+    public void testSetBalanceWithNegativeValue() {
+        BankAccount account = new BankAccount();
+        assertThrows(RuntimeException.class, () -> {
+            account.setBalance(-100.50f);
+        });
+    }
+
+    @Test
+    public void testGetBalance() {
+        BankAccount account = new BankAccount();
+        float expectedBalance = 2500.75f;
+        account.setBalance(expectedBalance);
+        assertEquals(expectedBalance, account.getBalance());
+    }
+
+    @Test
+    public void testBalanceFieldIsPrivate() {
+        Field[] fields = BankAccount.class.getDeclaredFields();
+        boolean balanceIsPrivate = false;
+        for (Field field : fields) {
+            if (field.getName().equals("balance")) {
+                balanceIsPrivate = Modifier.isPrivate(field.getModifiers());
+                break;
+            }
+        }
+        assertTrue(balanceIsPrivate, "Balance field should be private");
+    }
+
+    @Test
+    public void testRectangleGettersAndSetters() {
+        Rectangle rect = new Rectangle();
+        rect.setLength(7);
+        rect.setWidth(4);
+        assertEquals(7, rect.getLength());
+        assertEquals(4, rect.getWidth());
+    }
+
+    @Test
+    public void testRectangleAreaCalculation() {
+        Rectangle rect = new Rectangle();
+        rect.setLength(5);
+        rect.setWidth(3);
+        assertEquals(15, rect.getArea());
+    }
+
+    @Test
+    public void testRectangleZeroDimensions() {
+        Rectangle rect = new Rectangle();
+        rect.setLength(0);
+        rect.setWidth(0);
+        assertEquals(0, rect.getArea());
+    }
+
+    @Test
+    public void testRectangleNegativeLengthThrows() {
+        Rectangle rect = new Rectangle();
+        assertThrows(IllegalArgumentException.class, () -> rect.setLength(-1));
+    }
+
+    @Test
+    public void testRectangleNegativeWidthThrows() {
+        Rectangle rect = new Rectangle();
+        assertThrows(IllegalArgumentException.class, () -> rect.setWidth(-1));
     }
 }
