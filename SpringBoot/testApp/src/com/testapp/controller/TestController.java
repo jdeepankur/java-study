c0e17c300d55282254c6094b8c9c0992155f3be8
jdeepankur <jdeepankur@outlook.com>
2026-03-20 22:46:55 +0000

    finish spring hello world

--- SpringBoot/testApp/src/com/testapp/controller/TestController.java
+++ SpringBoot/testApp/src/com/testapp/controller/TestController.java
@@ -1,10 +1,13 @@
 package com.testapp.controller;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+  private int count;
 
   @GetMapping("/")
   public String index() {
@@ -12,8 +15,11 @@ public class TestController {
   }
 
   @GetMapping("/greeting") 
-  public String greeting(String name) {
-    return "test";
+  public Map greeting(String name) {
+    return Map.of(
+        "id", ++count,
+        "content", "Hello, " + name + "!"
+    );
   }
 
 }
