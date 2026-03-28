431620d1c46da7b126a95d1e0ecf5f019b56db16
Deepankur John Njondimackal <djn9@st-andrews.ac.uk>
2026-03-28 15:17:05 +0000

    found some edge cases for Q2

--- travelfusionmock/Q2.java
+++ travelfusionmock/Q2.java
@@ -1,12 +1,12 @@
 import java.util.*;
 import java.util.stream.Stream;
 
-// import com.google.gson.GsonBuilder;
+import com.google.gson.GsonBuilder;
 
 public class Q2 {
-    // public static void print(Object o) {
-    //     System.out.println(new GsonBuilder().setPrettyPrinting().create().toJson(o));
-    // }
+    public static void print(Object o) {
+        System.out.println(new GsonBuilder().setPrettyPrinting().create().toJson(o));
+    }
 
     public static boolean isBalanced(String html) {
         Tree root =  new Tree();
@@ -19,7 +19,16 @@ public class Q2 {
 
         for (char c:html.toCharArray()) {
             if (c == '<') {
-                if (!currTag.isEmpty()) {
+                if (!currText.isEmpty()) {
+                    Tree child = new Tree();
+                    child.parent = currNode;
+                    currNode.put("\"" + currText + "\"", child);
+                    child.closed = true;
+                    nodes.add(child);
+                    currText = "";
+                    currTag += c;
+                }
+                else if (!currTag.isEmpty()) {
                     return false;
                 }
                 else {
@@ -31,22 +40,35 @@ public class Q2 {
                 if (!closing) {
                     Tree child = new Tree();
                     child.parent = currNode;
-                    currNode.put(currTag, child);
-                    currNode = currNode.get(currTag);
+                    String tag = currTag.substring(currTag.indexOf('<') + 1, currTag.indexOf('>'));
+                    currNode.put(tag, child);
+                    currNode = currNode.get(tag);
                     nodes.add(currNode);
                 }
                 else {
                     currNode.closed = true;
                     currNode = currNode.parent;
                     closing = false;
+                    String tag = currTag.substring(currTag.indexOf('/') + 1, currTag.indexOf('>'));
+                    if (currNode == null || !currNode.containsKey(tag)) {
+                        return false;
+                    }
+                    else {
+                        print(currNode);
+                    }
                 }
 
                 currTag = "";
             }
             else {
-                currTag += c;
-                if (c == '/') {
-                    closing = true;
+                if (!currTag.isEmpty()) {
+                    currTag += c;
+                    if (c == '/') {
+                        closing = true;
+                    }
+                }
+                else {
+                    currText += c;
                 }
             }
         }
@@ -55,7 +77,7 @@ public class Q2 {
         for (Tree node:nodes) {
             result = result && node.closed;
         }
-        return result;
+        return result && currTag.isEmpty();
     }
 
     public static void main(String[] args) {
